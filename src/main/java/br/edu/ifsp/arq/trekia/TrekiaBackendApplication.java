b268059b7c1efcacee1205abd030d03477269e3d
Edenilson Junior <edenilsonju10@gmail.com>
2025-06-09 13:37:42 -0300

    initial commit

---
+++ src/main/java/br/edu/ifsp/arq/trekia/TrekiaBackendApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifsp.arq.trekia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrekiaBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrekiaBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/edu/ifsp/arq/trekia/TrekiaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifsp.arq.trekia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrekiaBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
